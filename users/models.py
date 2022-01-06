497073dc9f36c214187f6110066efe9266c64b11
thuynguyen650 <thuynguyen.webdev@gmail.com>
2022-01-06 16:02:05 +0700

    override the save method in profile model

--- users/models.py
+++ users/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     image = models.ImageField(default = 'profile_pics/default.jpg', upload_to = 'profile_pics')
     def __str__(self):
         return f'{self.user.username} profile'
+
+    # override the save method
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size) #resize img
+            img.save(self.image.path)
